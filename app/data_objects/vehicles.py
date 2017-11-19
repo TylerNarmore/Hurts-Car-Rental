fb136298071df182700a7e1742842c747581e660
Tyler <tyler@narmore.net>
2017-11-18 21:02:14 -0600

    Removed quantity

--- app/data_objects/vehicles.py
+++ app/data_objects/vehicles.py
@@ -1,8 +1,8 @@
 from redis import Redis
 
+
 def add_vehicle(vehicle):
     vehicleID = vehicle["vehicleID"]
-    quantity = vehicle["quantity"]
     make = vehicle["make"]
     model = vehicle["model"]
     year = vehicle["year"]
@@ -21,25 +21,26 @@ def add_vehicle(vehicle):
     #RedisCall
     return(1)
 
+
 def reset_vehicles_status():
     #Call redis to reset all vehicles back to their default.
     #AKA set quantity back to 1 and startDate and endDate to nothing
     pass
+
 
 def delete_vehicle(vehicleID):
     #Check if vehicle exists
     #Delete if it does and return a good message, return bad message if it doesn't
     pass
 
+
 def find_vehicle(search_terms):
     #Search terms is a dictionary of terms being searched with the category
     #Use these to search REDIS and return list of vehicles that match
     pass
 
+
 def purchase_vehicle(purchaseInformation):
     #Set vehicle quantity to 0 in redis
     #Do something to document it was purchased.
     pass
-
-
-
