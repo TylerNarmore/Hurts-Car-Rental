2ad32c692271c62c515e48e7e52490ddc13f2263
FlaxenTrousers <langolier009@gmail.com>
2017-12-01 21:34:58 -0600

    Version 1.1.2 of Hurts
    Pretty sure the website is finished, has some minor bugs, but nothing that breaks it. Should be good to go for demonstration purposes for the video per the assignment submission.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /*--------------------------------------------------------------------
  * main.js Handles API calls
  *--------------------------------------------------------------------*/
+ // HEY ASSHOLE ALERT THEM WHEN THEY RENT A CAR
 myAudio = new Audio('assets/sounds/bg_music.mp3');
 if (typeof myAudio.loop == 'boolean') {
 	myAudio.loop = true;
@@ -13,20 +14,131 @@ else {
 }
 myAudio.play();
 
+function googleMapAPI(cityState) {
+	var mapXHR = new XMLHttpRequest();
+
+	// Define what happens in case of error
+	mapXHR.addEventListener("error", function(event) {
+		alert('An error has occured.');
+	});
+
+	var mapXHR = new XMLHttpRequest();
+
+	var data;
+
+	mapXHR.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address="+ cityState + "&key=AIzaSyBjSWNyYv2Zm5r6BUjQgDZIxq7bEnoit1A", true);
+	mapXHR.send();
+	// Define what happens when data is returned from GET request
+	mapXHR.addEventListener("readystatechange", processMapRequest, false);
+
+
+function processMapRequest(event) {
+	if (mapXHR.readyState == 4 && mapXHR.status == 200) {
+		// Parse and store returned object
+		var response = JSON.parse(mapXHR.responseText);
+		data = response.results[0].geometry.location;	// For readability
+
+		console.log(data);
+		initMap(data)
+
+		}
+	} 
+}
+
+function initMap(data) {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 8,
+		center: data,
+		styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+	});
+	var marker = new google.maps.Marker ({
+		position: data,
+		map: map
+	});
+}
+
 window.addEventListener("load", function() {
 	var getForm = document.getElementById("getForm");
 	var elements = getForm.elements;
-
-	function initMap() {
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: data
-		});
-		var marker = new google.maps.Marker ({
-			position: data,
-			map: map
-		});
-	}
 
 	function sendData() {
 		// If an input was left blank disable it from form
@@ -34,16 +146,31 @@ window.addEventListener("load", function() {
 			if (element.value === "")
 				element.disabled = true;
 
-			else if (element.id == "location") {
-				if (element.value = "Home") {
+			else if (element.id == "location" && element.value != null) {
+				if (element.value === "Home") {
 					window.location.href = "http://softwarebois.com";
 				}
-			}
-
-			// Correctly format date values for search -- FIX THIS sober Josh
-			else if (element.id == 'start' || element.id == 'end') {
-				element.value = element.value + "T12:00:00";
-				console.log(element.value);
+				else if (element.value === "Moron Mountain") {
+					window.location.href = "http://spacejam.com";
+				}
+				else if (element.value === "Game Over") {
+					gameOver = new Audio('assets/sounds/gameOver.mp3');
+					gameOver.play();
+				}
+				else if (element.value === "Ice Cold") {
+					iceCold = new Audio('assets/sounds/ice.mp3');
+					iceCold.play();
+				}
+				else if (element.value === "Lisa") {
+					lisa = new Audio('assets/sounds/lisa.mp3');
+					lisa.play();
+				}
+				else {
+					var mapDiv = document.getElementById('map');
+					mapDiv.style.display = "block";
+					var cityState = element.value;
+					googleMapAPI(cityState);
+				}
 			}
 		}
 
@@ -71,14 +198,15 @@ window.addEventListener("load", function() {
 				// Loop for displaying data in separate divs
 				for (var each in data) {
 					var tempContainer = container.clone();
+					//tempContainer.children().atrr('disabled', 'disabled');
 					$('.make', tempContainer).text("Make: " + data[each].make);
 					$('.model', tempContainer).text("Model: " + data[each].model);
 					$('.year', tempContainer).text("Year: " + data[each].year);
 					$('.type', tempContainer).text("Type: " + data[each].type);
-					$('.location', tempContainer).text("Location: " + data[each].location);
+					$('.passengers', tempContainer).text("Passengers: " + data[each].passengers);
 					$('.cost', tempContainer).text("Cost: " + data[each].cost);
 					$('.mpg', tempContainer).text("MPG: " + data[each].mpg);
-					$('.passengers', tempContainer).text("Passengers: " + data[each].passengers);
+					$('.location', tempContainer).text("Location: " + data[each].location);
 
 					// Create form data for selecting start/end dates for rental
 					var postForm = document.createElement("form");
