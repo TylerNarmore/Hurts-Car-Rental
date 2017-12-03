249aad46b57e85483b9e84d9217748188f872b80
Tyler <tyler@narmore.net>
2017-12-02 21:12:51 -0600

    Server fixed to include admin page

--- app/server.py
+++ app/server.py
@@ -3,8 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/inventory.html')
+def inventory():
+    return render_template('inventory.html')
+
+
 
 @app.route('/christmas')
 def christmas():
